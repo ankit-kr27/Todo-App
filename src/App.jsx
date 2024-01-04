0ef6360b6f0d80561e4974ea8d3776f7ad097911
Ankit Kumar <98731902+ankit-kr27@users.noreply.github.com>
2024-01-04 09:29:28 +0530

    Defining functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react"
 import { TodoContextProvider } from "./contexts"
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo)=>{
+    setTodos(prev=>[{id:Date.now(), ...todo}, ...prev])
+  }
+  // assigning id automatically and rest of the information about the todo is passed on by the parameter
+
+  const updateTodo = (id, todo)=>{
+    setTodos((prev) => prev.map((curr)=>(
+      curr.id === id ? todo : curr
+    )))
+  }
+  // .map returns and maps what we return out of it into an array while iterating over each element, it accepts a callback containing the current element
+
+  const deleteTodo = (id)=>{
+    setTodos((prev)=>prev.filter((curr)=>(curr.id!==id)))
+  }
+  // .filter maps the returned value into an array which satisfies a certain condition. It works with a true condition.
+
+  const toggleTodo = (id)=>{
+    setTodos((prev)=>prev.map((curr)=>(
+      curr.id===id?{...curr, completed : true} : curr
+    )))
+  }
 
   return (
-    <TodoContextProvider value={{todos, addTodo, }}>
+    <TodoContextProvider value={{todos, addTodo, deleteTodo, updateTodo, toggleTodo}}>
       
     </TodoContextProvider>
   )
--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,3 +1,3 @@
 // You've multiple contexts you pick all of them and export them once via index.js
 
-export {todoContext, TodoProvider, useTodo} from './toDoContext'    // This is a sneaky syntax
+export {todoContext, TodoContextProvider, useTodo} from './toDoContext'    // This is a sneaky syntax
